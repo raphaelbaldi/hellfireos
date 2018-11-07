82961dee36bb53ded92a02e1914c1ac3265fbfdb
13180385 <13180385@facin.portoalegre.pucrsnet.br>
2018-11-07 20:44:17 -0200

    WIP

--- app/labyrinth/labyrinth.c
+++ app/labyrinth/labyrinth.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <hellfire.h>
+#include <noc.h>
 #include "labyrinth.h"
 
-void show(int *m, int lin, int col)
-{
+void show(int *m, int lin, int col) {
 	int i, j;
 	
 	for (i = 0; i < lin; i++) {
@@ -14,8 +14,7 @@ void show(int *m, int lin, int col)
 	printf("\n");
 }
 
-void mark(int *m, int l, int c, int lin, int col)
-{
+void mark(int *m, int l, int c, int lin, int col) {
 	int h, i, j, k = 1;
 	
 	for (h = 0; h < lin * col; h++, k++) {
@@ -23,20 +22,24 @@ void mark(int *m, int l, int c, int lin, int col)
 			for (j = 0; j < col; j++) {
 				if (m[i * col + j] == k) {
 					if (i - 1 >= 0) {
-						if (m[(i - 1) * col + j] == 0)
+						if (m[(i - 1) * col + j] == 0) {
 							m[(i - 1) * col + j] = k + 1;
+						}
 					}
 					if (i + 1 < lin) {
-						if (m[(i + 1) * col + j] == 0)
+						if (m[(i + 1) * col + j] == 0) {
 							m[(i + 1) * col + j] = k + 1;
+						}
 					}
 					if (j - 1 >= 0) {
-						if (m[i * col + (j - 1)] == 0)
+						if (m[i * col + (j - 1)] == 0) {
 							m[i * col + (j - 1)] = k + 1;
+						}
 					}
 					if (j + 1 < col) {
-						if (m[i * col + (j + 1)] == 0)
+						if (m[i * col + (j + 1)] == 0) {
 							m[i * col + (j + 1)] = k + 1;
+						}
 					}
 				}
 			}
@@ -44,13 +47,12 @@ void mark(int *m, int l, int c, int lin, int col)
 	}
 }
 
-int search(int *m, int i, int j, int ei, int ej, int lin, int col)
-{
+int search(int *m, int i, int j, int ei, int ej, int lin, int col) {
 	int k = 2;
 	
 	while (k > 1) {
 		k = m[i * col + j];
-		printf("[%d,%d] ", i, j);
+		/* printf("[%d,%d] ", i, j); */
 		if (i - 1 >= 0) {
 			if (m[(i - 1) * col + j] < k && m[(i - 1) * col + j] > 0) {
 				i--;
@@ -76,36 +78,79 @@ int search(int *m, int i, int j, int ei, int ej, int lin, int col)
 			}
 		}
 	}
-	if (i == ei && j == ej)
+	if (i == ei && j == ej) {
 		return 1;
-	else
+	} else {
 		return 0;
+	}
 }
 
-int solve(int *m, int lin, int col, int si, int sj, int ei, int ej)
-{
+int solve(int *m, int lin, int col, int si, int sj, int ei, int ej) {
 	m[ei * col + ej] = 1;
 	mark(m, ei, ej, lin, col);
-	/* show(m, lin, col); */
 	return search(m, si, sj, ei, ej, lin, col);
 }
 
-int main(void)
-{
+void slave(void) {
+
+}
+
+void master(void) {
 	struct maze_s *m;
-	int i, s, k = 0;
+	int i, s, k, j, initial, buf_size = 0;
 	
+	int32_t init = _readcounter();
+	
+	if (hf_comm_create(hf_selfid(), 5000, 0)) {
+		panic(0xff);
+	}
+	
+	///*
+	int maze_count = sizeof(mazes) / sizeof(struct maze_s);
+	
+	while (i < maze_count) {
+	    // Send to all
+	    initial = i;
+	    for (j = 1; j < hf_ncores() && i < maze_count; j++) {
+    	    m = &mazes[i];
+    	    buf_size = (m->lines * m->columns + 6) * sizeof(int);
+	        val = hf_send(j, j + 5000, m, buf_size, 0);
+		    if (val) {
+		        printf("hf_send(): error %d\n", val);
+		    } else {
+		        printf("hf_send(): [%d] sent a message to [%d:%d], on channel [%d], size: %d\n", hf_selfid(), j, j + 5000, 0, buf_size);
+		    }
+	        i++;    
+	    }
+	    
+	    // Receive from all
+	    for (j = 1; j < hf_ncores(); j++) {
+	        
+	    }
+	}
+	//*/
+	
+	/*
 	for (i = 0; i < sizeof(mazes) / sizeof(struct maze_s); i++) {
 		m = &mazes[i];
 		s = solve(m->maze, m->lines, m->columns, m->start_line, m->start_col, m->end_line, m->end_col);
 		if (s) {
-			printf("\nOK!\n");
+			printf("\n[%d] OK!", i);
 			k++;
 		} else {
-			printf("\nERROR!\n");
+			printf("\n[%d] ERROR!", i);
 		};
 	};
-	printf("\nsummary: %d of %d solved\n", k, i);
+	//*/
+	int32_t delta = _readcounter() - init;
+	printf("\nsummary: %d of %d solved [%d]\n", k, i, delta);
 	
-	return 0;
+	while (1) {
+	}
+}
+
+void app_main(void) {
+    if (hf_cpuid() == 0) {
+	    hf_spawn(master, 0, 0, 0, "Solver", 2048);
+	}
 }
