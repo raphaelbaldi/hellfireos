dc3868d6faade55a6a951c667ff73fab8496275f
Raphael Baldi <raphaelbaldi@gmail.com>
2018-11-13 13:45:06 -0200

    Final implementation, including fix to stack overflow
    And fix to never stopping slaves

--- app/labyrinth/labyrinth.c
+++ app/labyrinth/labyrinth.c
@@ -109,10 +109,14 @@ void slave(void) {
 	int16_t val;
 	int8_t buf[1792], maze_id, lines, columns, start_line, start_column, end_line, end_column, result;
 	
+	int port = 5000 + hf_cpuid();
+	
 	// Initialize network communications
-	if (hf_comm_create(hf_selfid(), 5000 + hf_cpuid(), 0)) {
+	if (hf_comm_create(hf_selfid(), port, 0)) {
 		panic(0xff);
 	}
+	
+	printf("Task [%d] listening on port [%d].\n", hf_selfid(), port);
 	
 	int running = 1;
 	while(running) {
@@ -159,6 +163,7 @@ void slave(void) {
 		} else if (buf[0] == STOP_RUNNING_EVENT) {
 			// Stop running
 			printf("[%d] Will stop running jobs since no more jobs available.\n", hf_selfid());
+			running = 0;
 		} else {
 			printf("[%d] received an unknown protocol code [%d] from [%d@%d], with size %d\n", hf_selfid(), buf[0], task, cpu, size);
 		}
@@ -177,24 +182,77 @@ void master(void) {
 	// If slave is asking for job, and we have jobs to do, send it to
 	//    the slave. Otherwise, notify to stop.
 	// If the slave is sending a solution back, store it.
+	int8_t maze_count = sizeof(mazes) / sizeof(struct maze_s);
+	int8_t current_maze = 0;
+	int8_t solved_mazes = 0;
 	
-	struct maze_s *m;
-	int i, s, k, j, initial, buf_size = 0;
+	int8_t buf[1792];
+	uint16_t cpu, task, size;
+	int16_t val;
+	int i;
 	
-	int32_t init = _readcounter();
+	struct maze_s * m;
 	
 	if (hf_comm_create(hf_selfid(), 5000, 0)) {
 		panic(0xff);
 	}
 	
-	int maze_count = sizeof(mazes) / sizeof(struct maze_s);
+	int32_t init = _readcounter();
 	
-	
-	
+	while(solved_mazes < maze_count) {
+		// Read the network
+		val = hf_recv(&cpu, &task, buf, &size, 0);
+		if (val) {
+			printf("hf_recv(): [%d] error %d\n", hf_selfid(), val);
+		} else {
+			printf("hf_recv(): [%d] received a message from [%d@%d], with size %d\n", hf_selfid(), task, cpu, size);
+		}
+		
+		if (buf[0] == GET_JOB_REQUEST) {
+			if (current_maze >= maze_count) {
+				// Tell the slave to shut down
+				buf[0] = STOP_RUNNING_EVENT;
+				val = hf_send(cpu, task, buf, 1, 0);
+				if (val) {
+					printf("hf_send(): error %d\n", val);
+				} else {
+					printf("hf_send(): [%d] master node told [%d@%d] to stop running.\n", hf_selfid(), task, cpu);
+				}
+			} else {
+				m = &mazes[current_maze];
+				
+				// Send a job to slave
+				printf("Slave [%d@%d] is requesting a job. Jobs left: %d/%d.\n", task, cpu, maze_count - current_maze, maze_count);
+				buf[0] = GET_JOB_RESPONSE;
+				buf[1] = current_maze;
+				buf[2] = m->lines;
+				buf[3] = m->columns;
+				buf[4] = m->start_line;
+				buf[5] = m->start_col;
+				buf[6] = m->end_line;
+				buf[7] = m->end_col;
+				for (i = 0; i < m->lines * m->columns; i++) {
+					buf[i + 8] = m->maze[i];
+				}
+				val = hf_send(cpu, 5000 + cpu, buf, 8 + m->lines * m->columns, 0);
+				if (val) {
+					printf("hf_send(): error %d\n", val);
+				} else {
+					printf("hf_send(): [%d] Master node sent maze [%d] to slave [%d@%d].\n", hf_selfid(), current_maze, task, cpu);
+				}
+				++current_maze;
+			}
+		} else if (buf[0] == SOLUTION_EVENT) {
+			// The slave solved a maze.
+			++solved_mazes;
+			printf("Received a solution for maze [%d] from [%d@%d]: %d. Solved mazes: %d/%d.\n", buf[1], task, cpu, buf[2], solved_mazes, maze_count);
+		}
+	}
 	
 	int32_t delta = _readcounter() - init;
-	printf("\nsummary: %d of %d solved [%d]\n", k, i, delta);
+	printf("\nsummary: %d of %d solved [%d]\n", solved_mazes, maze_count, delta);
 	
+	// Wait forever
 	while (1) {
 	}
 }
@@ -202,8 +260,8 @@ void master(void) {
 void app_main(void) {
 	// The first CPU runs the master task, the others, the slave task.
     if (hf_cpuid() == 0) {
-	    hf_spawn(master, 0, 0, 0, "Master Task", 2048);
+	    hf_spawn(master, 0, 0, 0, "Master Task", 4096);
 	} else {
-		hf_spawn(slave, 0, 0, 0, "Slave Task", 2048);
+		hf_spawn(slave, 0, 0, 0, "Slave Task", 4096);
 	}
 }
